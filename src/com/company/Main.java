117d2d2b82829fef77a32efa316c7c773d5bc3cd
Illya Pashchenko <illya@MacBook-Pro.local>
2020-09-23 19:50:13 +0300

    mergeproblem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public String fixstring = "Hello from user2";
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello merge problem");
     }
 }
